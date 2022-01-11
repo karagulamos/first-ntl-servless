8ad55800a6a38fca59eed2c56a60a81341308e9b
Alexander Karagulamos <karagulamos@gmail.com>
2022-01-12 00:13:19 +0100

    feature: add post requests to pokedex api

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,7 +1,10 @@
 import fetch from 'node-fetch'
 
-export async function handler() {
-    const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
+export async function handler(event, context) {
+    const eventBody = JSON.parse(event.body)
+    const POKE_API = `https://pokeapi.co/api/v2/pokedex/${eventBody.region}`
+
+    console.log(POKE_API)
 
     const response = await fetch(POKE_API)
     const data = await response.json()
@@ -9,7 +12,7 @@ export async function handler() {
     return {
         statusCode: 200,
         body: JSON.stringify({
-            data: data
+            pokemon: data.pokemon_entries
         })
     }
 }
